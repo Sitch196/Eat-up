eca4a8bf88570f64f67df3ad992623a6a306fcf1
Sitch196 <sichinava1996@gmail.com>
2023-01-29 22:01:38 +0400

    added button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import HeaderImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header() {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Eat Up</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={HeaderImage} />
