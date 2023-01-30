3e56fbc76d4d3ea172668b0bb293120756c22229
Sitch196 <sichinava1996@gmail.com>
2023-01-30 15:57:54 +0400

    added meals and its components

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -6,7 +6,7 @@ function HeaderCartButton() {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span>Cart</span>
       <span className={classes.badge}>4</span>
     </button>
   );
